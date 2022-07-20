97688f982f39bf858043388b734686254f0d958d
38krupaliyadav <77061355+38krupaliyadav@users.noreply.github.com>
2022-07-20 12:34:45 +0200

    Add files via upload

---
+++ js/stone.js
@@ -0,0 +1,12 @@
+class stone{
+    constructor(x,y)
+{
+    var options={
+        isStatic:false,
+        restitution:0,
+        friction:1, 
+        density:1.2
+    }
+    this.image=loadImage("images/stone.png")
+}
+}
